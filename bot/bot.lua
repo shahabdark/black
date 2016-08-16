3c7323b18dd96fb03822bb9f2e25f0d3fb1ce716
iiborn <abbasgit91@gmail.com>
2016-08-16 11:06:56 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -263,7 +263,7 @@ function create_config( )
     "Block",
     "InGroups",
     },
-    sudo_users = {56693692,},--Sudo users
+    sudo_users = {251862863,},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 ]],
