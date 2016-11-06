c538ce14794052438f4041abaed846f8f7831b53
iiborn <abbasgit91@gmail.com>
2016-11-06 14:03:05 +0330

    Update Webshot.lua

--- plugins/Webshot.lua
+++ plugins/Webshot.lua
@@ -1,14 +1,3 @@
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
 local helpers = require "OAuth.helpers"
 
 local base = 'https://screenshotmachine.com/'
@@ -66,14 +55,3 @@ return {
    },
    run = run
 }
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
