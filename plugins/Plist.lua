0f39c1d52a1eee63caf5d7471af4c332384b8e17
iiborn <abbasgit91@gmail.com>
2016-11-06 14:26:20 +0330

    Update Plist.lua

--- plugins/Plist.lua
+++ plugins/Plist.lua
@@ -1,20 +1,9 @@
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
 do
 
 local function run(msg, matches)
   if matches[1] == 'plist' or 'planlist' then
-    send_document(get_receiver(msg), "/root/blackplus/data/me/payment.webp", ok_cb, false)
-      return "For more information please Contact this robot \n\n@BlackSupport_Bot"
+    send_document(get_receiver(msg), "/root/black/data/me/payment.webp", ok_cb, false)
+      return "For more information please Contact this robot \n\n@ibornbot"
 end
 end
 return {
@@ -28,14 +17,3 @@ return {
   run = run
 }
 end
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
