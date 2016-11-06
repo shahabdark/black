aca379af28bc773e7a9cda0ef64d119568611e69
iiborn <abbasgit91@gmail.com>
2016-11-06 13:13:50 +0330

    Update and rename BlackPlus.lua to Extreme.lua

--- plugins/Extreme.lua
+++ plugins/Extreme.lua
@@ -1,14 +1,3 @@
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
 do
 
 -- Returns a table with `name` and `msgs`
@@ -102,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'blackplus' then -- Put everything you like :)
+  if matches[1]:lower() == 'extreme' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /blackplus ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /extreme ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -132,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "blackplus" then
+    if matches[2] == "extreme" then
       if not is_sudo(msg) then
         return "*You're Not Allowed."
       else
@@ -154,23 +143,12 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (blackplus)",
+    "^[#!/]([Ss]tats) (extreme)",
     "^([Ss]tats)$",
     "^([Ss]tatslist)$",
     "^([Ss]tats) (group) (%d+)",
-    "^([Ss]tats) (blackplus)",
+    "^([Ss]tats) (extreme)",
     }, 
   run = run
 }
 end
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
