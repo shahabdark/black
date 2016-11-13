db5304ffab12c91ea83ba169d17af19f5fc83b34
iiborn <abbasgit91@gmail.com>
2016-11-13 14:58:44 +0330

    Update mean.lua

--- plugins/mean.lua
+++ plugins/mean.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
   local htp = http.request('http://api.vajehyab.com/v2/public/?q='..URL.escape(matches[1]))
   local data = json:decode(htp)
- return 'کلمه : '..(data.data.title or data.search.q)..'\n\nمعنی : '..(data.data.text or '----' )..'\n\nمنبع : '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..''
+ return 'کلمه : '..(data.data.title or data.search.q)..'\n\nمعنی : '..(data.data.text or '----' )..'\n\nمنبع : '..(data.data.source or '----' )..'\n\n'..(data.error.message or '')..'\n'
 end
 return {
   patterns = {
