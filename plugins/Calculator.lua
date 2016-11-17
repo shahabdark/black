7e2c3680a3b2f2d7e063ff4259577078f7fc428a
shahabdark <shahabt1380@gmail.com>
2016-11-17 21:55:56 +0330

    Update Calculator.lua

--- plugins/Calculator.lua
+++ plugins/Calculator.lua
@@ -4,7 +4,7 @@ local function mathjs(exp)
   local b,c = http.request(url)
   local text = nil
   if c == 200 then
-    text = 'Result: '..b
+    text = '🔢 Result : '..b
   
   elseif c == 400 then
     text = b
