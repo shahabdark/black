cef6841680ea25d4331993796a953cb7753a5619
shahabdark <shahabt1380@gmail.com>
2016-11-17 22:04:25 +0330

    Update Del_Pro.lua

--- plugins/Del_Pro.lua
+++ plugins/Del_Pro.lua
@@ -3,16 +3,16 @@ local function history(extra, suc, result)
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" #Done. ', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'" ✅ Done. ', ok_cb, false)
   else
-    send_msg(extra.chatid, '"'..#result..'" #Done. ', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'" ✅ Done. ', ok_cb, false)
   end
 end
 local function run(msg, matches)
   if matches[1] == 'del' and is_owner(msg) then
     if msg.to.type == 'channel' then
       if tonumber(matches[2]) > 99 or tonumber(matches[2]) < 2 then
-        return "*Wrong number,range is [2-99]"
+        return "⚠️ Wrong number,range is [2-99]"
       end
       get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
     else
