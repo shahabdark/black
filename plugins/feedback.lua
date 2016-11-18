a1b28a947bc85abf9a649dbece4200ee796cdda3
shahabdark <shahabt1380@gmail.com>
2016-11-18 03:30:47 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,11 +1,11 @@
 function run(msg, matches)
  
- local fuse = 'New FeedBack Just We Received From User '..msg.from.username..' | '..msg.from.id..'\nUser#ID : ['..msg.from.id..']\nName : ' .. msg.from.print_name ..'\nUsername : @' .. msg.from.username .. '\n\nLocal Message:\n\n' .. matches[1] 
+ local fuse = 'New FeedBack Just We Received \n\n👤 From User '..msg.from.username..'\n\n💠 User ID : ['..msg.from.id..']\n\n💠 Name : ' .. msg.from.print_name ..'\n\n💠 Username : @' .. msg.from.username .. '\n\n📩 Local Message:\n\n' .. matches[1] 
  local fuses = '!printf user#id' .. msg.from.id
  
  
    local text = matches[1]
-   local chat = "user#id"..251862863
+   local chat = "user#id"..275387751
    --like : local chat = "chat#id"..56693692
    
   local sends = send_msg(chat, fuse, ok_cb, false)
